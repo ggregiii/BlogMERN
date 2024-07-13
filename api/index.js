f845b3fb2f7ea3394dc14529a6ff74223457dfe2
Garfield <lim.ggreg@gmail.com>
2024-07-13 16:25:20 +0800

    Implemented returning of data at 54:43

--- api/index.js
+++ api/index.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/test', (req, res) => {res.json('test okzs')})
 
 // create /register endpoint app.post
-app.post('/register', (req, res) => {res.json("register okzs");
+app.post('/register', (req, res) => {
+    
+    const{username, password} = req.body;
 
+    res.json({requestData:{username, password}})
 });
 
 app.listen(4000);
