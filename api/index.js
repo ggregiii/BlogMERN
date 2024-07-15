772c9f4186abc068c50f8bf10fa8514d6f139e40
Garfield <lim.ggreg@gmail.com>
2024-07-15 15:18:53 +0800

    Implemented log-in until 1:41:08 need logout fix

--- api/index.js
+++ api/index.js
@@ -44,7 +44,10 @@ app.post("/login", async (req, res) => {
   if (passOk) {
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie("token", token).json("ok");
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
     });
   } else {
     res.status(400).json("Wrong credentials");
@@ -58,6 +61,10 @@ app.get("/profile", (req, res) => {
     if (err) throw err;
     res.json(info);
   });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
 app.listen(4000);
